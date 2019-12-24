838a52a19375e12360315d8d0a843a8f7453e598
katunold <katunold94@gmail.com>
2019-12-24 12:15:26 +0300

    - add performance optimization

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -81,4 +81,19 @@ $(document).ready(function () {
   }, {
     offset: '50%'
   });
+
+  //  Mobile nav
+  $('.js--nav-icon').click(function () {
+    const nav = $('.js--main-nav');
+    const icon = $('.js--nav-icon ion-icon');
+    nav.slideToggle(200);
+    if (icon.hasName('menu')) {
+      icon.addName('close');
+      icon.removeName('menu')
+    } else {
+      icon.addName('menu');
+      icon.removeName('close')
+    }
+  });
+
 });
